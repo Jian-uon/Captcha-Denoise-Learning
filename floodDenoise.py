ba0a61bf87d7126de158b80f1fcd736ba1444190
usedrose <735342728@qq.com>
2018-10-16 11:11:28 +0800

    添加二值化函数

--- floodDenoise.py
+++ floodDenoise.py
@@ -1,10 +1,13 @@
 #coding:utf-8
+#洪水填充法去除大的噪点
 import numpy as np
 from PIL import Image
 import sys
 
 
+#是我dfs写的有问题吗。。不加最大深度会爆栈，真的是有问题。。
 sys.setrecursionlimit(160*60)
+#全局定义太多差评。。。
 filename = '2.jpg'
 img = Image.open(filename)
 img = img.convert('L')
@@ -17,7 +20,7 @@ threshold = 230
 zone_threshold = w*h/100
 zone = [0]*w*h
 
-def flood_denoise(img):
+def flood_fill_denoise(img):
     print img.size
     index = 1
     for i in xrange(w):
@@ -47,8 +50,8 @@ def dfs(x, y, index):
         return
     visited[x][y] = index
     zone[index] += 1 
-    dx = [-1, -1, 1, 1]
-    dy = [-1, 1, -1, 1]
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
     for i in xrange(4):
         nx = x+dx[i]
         ny = y+dy[i]
@@ -59,8 +62,20 @@ def dfs(x, y, index):
                 visited[nx][ny] = 0
     return
 
+#二值化图片
+#img为Image类型 b为阈值
+def getBinaryImage(img, b):
+    for i in xrange(img.size[0]):
+        for j in xrange(img.size[1]):
+            if img.getpixel((i,j)) > b:
+                img.putpixel((i,j), 255)
+            else:
+                img.putpixel((i,j), 0)
+    return img
+
 if __name__ == '__main__':
-    print np.array(img)
-    new_img = flood_denoise(img)
-    new_img.save('f'+filename)
+    img = getBinaryImage(img, threshold)
+    img.save('binary'+filename)
+    new_img = flood_fill_denoise(img)
+    new_img.save('result'+filename)
     new_img.show()
